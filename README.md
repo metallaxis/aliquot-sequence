2f0540ce44be859f32cdd9ff539690a9057650b6
Georgios Chatzopoulos <george@metallaxis.net>
2025-10-16 19:04:36 +0300

    Updating aliquot file to work with bigger numbers by using the square root of the number

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 This is a program that tries to follow the aliquot sequence but has a limit of 10^15 integer limit.
-Also this version is a bit slow for big numbers.
 
 # Compiling
 In order to compile you need to install gcc.
 Debian Linux: sudo apt-get install gcc
 
-And then compile it using gcc aliquot.c -o aliquot
+And then compile it using gcc aliquot.c -o aliquot -lm
--- aliquot.c
+++ aliquot.c
@@ -1,22 +1,18 @@
-/*
-
-    This program embodies the aliquot sequence
-
-*/
-
+// This program embodies the aliquot sequence
 // Required inclusions from the C library
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <math.h>
 
 // Main Method
 int main(int argc, char **argv) {
-    // Initialize the checker variable
+    // Declaring the checker variable
     char term;
 
-    // Initialize the variables for the program
+    // Declaring the variables for the program
     uint64_t number;
     size_t length;
     char sequence;
@@ -34,6 +30,8 @@ int main(int argc, char **argv) {
         printf("The value entered is not of type integer. Stopping.\n"); // Show error if the variable is not an integer
         exit(1); // Exit the program with exit code 1
     }
+
+    //Prompt for whether it should show the full sequence or just its length
     printf("Do you want to print the full sequence ('f') or just the length ('l')? ");
     scanf("%c", &sequence);
     if (sequence != 'f' && sequence != 'l') { // Check whether the value is 'l' or 'f'
@@ -41,33 +39,51 @@ int main(int argc, char **argv) {
         exit(1); // Exit the program with exit code 1
     }
 
+    uint64_t initial_number = number; // Number before changes useful for debugging
     size_t num = 0; // Initialize the variable for the loop necessery if the length from before is not 0
-    do { // Run once then check the loop
-        if (length != 0) ++num;
+    char run = 1;
 
-        if (number > 1e15) {
-            printf("Number exceeds maximum supported integer (1000000000000000). Stopping.\n");
+    while (run) { //If run is 1 run otherwise stop
+        if (number == 0 || (length == 0 ? 0 : num >= length)) {
+            run = 0;
+            break;
+        }
+
+        ++num;
+
+        if (sequence == 'f') printf("%" PRIu64 "\n", number); // Print the value of number
+
+        if (number > 1e15) { // Check if the number is above 10^15
+            printf("Number exceeds maximum supported integer (1000000000000000). Stopping.\n"); // Show an error if its
             exit(1); // Exit the program with exit code 1
         }
 
-        uint64_t final = 0;
+        uint64_t final = 0; // Set the default final number
+        uint64_t sqrt_num = (uint64_t) sqrt(number);
 
-        for (uint64_t i = 1; i < number; ++i) {
-            if (number % i == 0) final+=i;
+        for (uint64_t i = 1; i <= sqrt_num; ++i) { // Run for every number from 1 to the square root of the number
+            if (number % i == 0) { // Check if the number is divisible by i
+                uint64_t paired = number / i; // Get the paired number that if multiplied with i gets the number
+
+                if (i != number) final += i;  // Add the small divisor
+        
+                // Add the paired divisor if it's different from i and not the number itself
+                if (paired != i && paired != number) final += paired;
+            }
+            
             if (argc > 1 && (strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--debug") == 0)) // Debug
-                printf("Final: %" PRIu64 ", I: %" PRIu64 "\n", final, i);
+                printf("I: %" PRIu64 ", Final: %" PRIu64 "\n", i, final);
         }
 
-        if (sequence == 'f') printf("%" PRIu64 "\n", number);
-        if (sequence == 'f' && number == 1) printf("%" PRIu64 "\n", final);
-        number = final;
-    } while ((length == 0 ? 1 : num < length) && number != 0); //If length is 0 run infinetely otherwise run until the num hits the length
+        if (sequence == 'f' && number == 1) printf("%" PRIu64 "\n", final); // Print the value of final if the number is 1
+        number = final; //Assign the final value to the number to rerun with the new value
+    }
 
-    if (sequence == 'l') printf("Length of aliquot sequence: %lu", num);
+    if (sequence == 'l') printf("Length of aliquot sequence: %lu\n", num); //If the choice 'l' is selected print the length of the sequence
 
     // DEBUG
     if (argc > 1 && (strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--debug") == 0)) {
-        printf("Number: %" PRIu64 "\n", number);
+        printf("Number: %" PRIu64 "\n", initial_number);
         printf("Length: %lu\n", length);
         printf("Sequence: %c\n", sequence);
     }
